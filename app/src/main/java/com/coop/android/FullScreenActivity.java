958b4836e31e1be5d25f3065796010811c659097
Mr-Z <13301365800@126.com>
2018-12-06 17:32:05 +0800

    coop create

---
+++ app/src/main/java/com/coop/android/FullScreenActivity.java
@@ -0,0 +1,13 @@
+package com.coop.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FullScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen);
+    }
+}
